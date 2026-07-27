65afc3fa675b8eff0f07731a2b83e9155781e151
Kwizera250232 <kwizerajeandedieu250@gmail.com>
2026-07-27 23:59:12 +0200

    Bump service worker cache version so clients receive the new build

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'uclass-v35';
+const CACHE_VERSION = 'uclass-v36';
 const API_CACHE = 'uclass-api-v2';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
