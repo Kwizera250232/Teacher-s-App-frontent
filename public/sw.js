15c531b81fddc4c5f62465287e5efe58db5d3eb1
Kwizera250232 <kwizerajeandedieu250@gmail.com>
2026-08-24 22:40:06 +0200

    Fix mobile table scroll - override parent overflow-x hidden, use overflow-x scroll

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'uclass-v53';
+const CACHE_VERSION = 'uclass-v54';
 const API_CACHE = 'uclass-api-v2';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
