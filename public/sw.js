ba5e8174abe252f64ae2ab410e2297cfb912b1bb
Kwizera250232 <kwizerajeandedieu250@gmail.com>
2026-08-24 21:19:36 +0200

    Force roster table horizontal scroll on mobile - override parent overflow-x hidden

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'uclass-v50';
+const CACHE_VERSION = 'uclass-v51';
 const API_CACHE = 'uclass-api-v2';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
